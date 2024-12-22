35bb4e6e449003e91dcce1d730288f87b6b916ea
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2024-12-22 20:52:47 +0700

    Create TwoSum BinarySearch.cpp

---
+++ TwoSum BinarySearch.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+bool binarySearch(vector<int> a, int l, int r, int x)
+{
+	while (l <= r)
+	{
+		int m = (l + r) / 2;
+		if (a[m] == x)
+		{
+			return true;
+		}
+		else if (a[m] < x)
+		{
+			l = m + 1;
+		}
+		else
+		{
+			r = m - 1;
+		}
+	}
+	return false;
+}
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	for (int i = 0; i < n; i++)
+	{
+		if (binarySearch(a, i + 1, n - 1,k - a[i]))
+		{
+			cout << "YES" << endl;
+			return 0;
+		}
+	}
+	cout << "NO" << endl;
+	return 0;
+}
