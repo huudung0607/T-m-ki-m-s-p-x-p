66752829d3b7e1959f5fa4596a98ea9163aa4396
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2024-12-29 19:24:46 +0700

    Create Bersu ball.cpp

---
+++ Bersu ball.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n), b(m);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cin >> b[i];
+	}
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	int i = 0, j = 0;
+	int dem = 0;
+	while (i < n && j < m)
+	{
+		if (abs(a[i] - b[j]) <= 1)
+		{
+			dem++;
+			i++;
+			j++;
+		}
+		else if (a[i] < b[j])
+		{
+			i++;
+		}
+		else
+		{
+			j++;
+		}
+	}
+	cout << dem;
+}
