1fc3d7a36a3b4e837eb38e3db310e7d0867dfc8f
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-04 20:47:35 +0700

    Create Sắp xếp nổi bọt.cpp

---
+++ Sắp xếp nổi bọt.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+using namespace std;
+
+void bubbleSort(vector<int> &a, int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0 ; j < n - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}	
+		}	
+		cout << "Buoc " << i + 1 << ": ";
+		for (int i = 0; i < n; i++)
+		{
+			cout << a[i] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	bubbleSort(a, n);
+}
